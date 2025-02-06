e2818954c4a29fc2ec842feaae345b89aad2c636
Kenneth Olivas <kennetholivas1@gmail.com>
2025-02-06 16:08:35 -0600

    init

---
+++ apps/frontend/.next/static/chunks/src_app_page_tsx_02d195._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/src_app_page_tsx_02d195._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/src_app_page_tsx_02d195._.js",
+  "chunks": [
+    "static/chunks/4d611_next_dist_16e030._.js",
+    "static/chunks/src_app_page_tsx_57ee28._.js"
+  ],
+  "source": "dynamic"
+});

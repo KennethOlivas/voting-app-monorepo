df390a73a9678ce317a9844ecaff13a4e244b0a7
Kenneth Olivas <kennetholivas1@gmail.com>
2025-03-04 22:29:11 -0600

    feat(voters): enhance voters feature with new table components and pagination

--- apps/frontend/src/components/features/voters/index.tsx
+++ apps/frontend/src/components/features/voters/index.tsx
@@ -1,19 +1,18 @@
 "use client"
 import React from 'react'
 import CreateVoter from '@/components/features/voters/create-voter'
-import { useGetVoters } from '@/hooks/queries/voters'
+import VotersTable from './voters-table'
 
 const Voters = () => {
-  const { data, isLoading, isFetching } = useGetVoters()
-  console.log(data, isLoading, isFetching)
+
   return (
-    <>
+    <div className='flex flex-col space-y-2'>
       <div className='flex items-center justify-between'>
         <h1 className='text-2xl font-bold tracking-tight'>Voters</h1>
         <CreateVoter />
       </div>
-      {JSON.stringify(data)}
-    </>
+      <VotersTable />
+    </div>
   )
 }
 
